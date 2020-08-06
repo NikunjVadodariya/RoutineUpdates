9d353af23d641898387a0e9680f5e6a1e49f4be4
NikunjVadodariya <58800184+NikunjVadodariyaMaruti@users.noreply.github.com>
2020-08-06 21:00:20 +0530

    made it better

--- client/js/components/components/Home.js
+++ client/js/components/components/Home.js
@@ -32,10 +32,10 @@ export default class Home extends React.Component {
           items: 1
         },
         600: {
-          items: 2
+          items: 8
         },
         1024: {
-          items: 3
+          items: 4
         }
       };
         return (
